dbb914804b7735ac021e1dcc5497416fee7453da
RanchMonster <RanchMonster06@outlook.com>
2025-02-10 17:00:23 -1000

    created the start_multitasking function this will be where all the branchs split off to work so there is no need for threading or multiprocessing keeping it safe and simple

--- CTE-Search/__main__.py
+++ CTE-Search/__main__.py
@@ -1,5 +1,14 @@
-from cli import Interface
-import curses
 
+from cli import Interface
+from asyncio import create_task, sleep, run, gather
+from LogManager import *
+from Cache import CacheHandle
+async def start_multitasking():
+    # LOGGER MUST START FIRST FOR LOGGING TO WORK
+    logger_task = create_task(logger_loop()) #Start the Logger loop in a separate task
+    inter = Interface()
+    interface_task = create_task(inter.run())  # Start curses in a separate task
+    
+    await gather(interface_task,logger_task)
 if __name__ == "__main__":
-    curses.wrapper(lambda stdscr: Interface().run(stdscr))
+    run(start_multitasking())
